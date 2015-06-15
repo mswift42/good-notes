ed3872f00f524fc47aa44d8b5133d03faf2de4b6
martin.haesler <martin.haesler@gmail.com>
2015-06-15 20:52:02 +0100

    Add id field / constructor

--- app/src/main/java/mswift42/com/github/good_notes/Task.java
+++ app/src/main/java/mswift42/com/github/good_notes/Task.java
@@ -1,10 +1,23 @@
 package mswift42.com.github.good_notes;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class Task {
     private String mSummary;
     private Date mScheduled;
+    private UUID mID;
+
+    Task(String summary, Date scheduled) {
+        this.mSummary = summary;
+        this.mScheduled = scheduled;
+        mID = UUID.randomUUID();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
 
     public Date getScheduled() {
         return mScheduled;
