26725596d3563594fbae87f6be2fdbfacaa5e516
martin.haesler <martin.haesler@gmail.com>
2015-06-15 20:45:40 +0100

    Generate getters/setters for scheduled, summary.

--- app/src/main/java/mswift42/com/github/good_notes/Task.java
+++ app/src/main/java/mswift42/com/github/good_notes/Task.java
@@ -5,4 +5,20 @@ import java.util.Date;
 public class Task {
     private String mSummary;
     private Date mScheduled;
+
+    public Date getScheduled() {
+        return mScheduled;
+    }
+
+    public void setScheduled(Date scheduled) {
+        mScheduled = scheduled;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
 }
