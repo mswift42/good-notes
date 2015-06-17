6535fb7e3a795ea378610be09a4b54547687c99e
martin.haesler <martin.haesler@gmail.com>
2015-06-17 13:23:08 +0100

    check getter/setter for task summary.

--- app/src/androidTest/java/mswift42/com/github/good_notes/TaskTest.java
+++ app/src/androidTest/java/mswift42/com/github/good_notes/TaskTest.java
@@ -2,12 +2,16 @@ package mswift42.com.github.good_notes;
 import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class TaskTest {
     @Test
     public static void task_intitializes_correctly() {
         Task t1 = new Task();
-        assertThat((t1.getID() != null), is(false));
+        assertFalse((t1.getID() == null));
+        t1.setSummary("task1");
+        assertEquals(t1.getSummary(), "task1");
     }
 }
 
