a10ec1a1ecff9fb0f7ec2cbc45f70486c5a09493
martin.haesler <martin.haesler@gmail.com>
2015-06-17 19:11:27 +0100

    generate NewTaskActivity.

---
+++ app/src/main/java/mswift42/com/github/good_notes/NewTaskActivityFragment.java
@@ -0,0 +1,22 @@
+package mswift42.com.github.good_notes;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NewTaskActivityFragment extends Fragment {
+
+    public NewTaskActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_task, container, false);
+    }
+}
